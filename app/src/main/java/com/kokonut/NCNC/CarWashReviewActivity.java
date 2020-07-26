4e86bfb356dca2327d698bd1e91a241ff04aa6b9
choiiis <haemin0128@naver.com>
2020-07-26 19:24:47 +0900

    update bottom navigation view

--- app/src/main/java/com/kokonut/NCNC/CarWashReviewActivity.java
+++ app/src/main/java/com/kokonut/NCNC/CarWashReviewActivity.java
@@ -1,50 +1,51 @@
 package com.kokonut.NCNC;
 
 import android.content.Intent;
-import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
-public class CarWashInfoActivity extends AppCompatActivity {
-    ImageView ivBack, ivOnButton, ivOnButton2, ivOnButton3, ivReview;
+public class CarWashReviewActivity extends AppCompatActivity {
+    ImageView ivBack, ivWriteReview;
 
     public static final int sub = 1001;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_car_wash_info);
+        setContentView(R.layout.activity_car_wash_review);
         initView();
 
         ivBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), MapActivity.class);
+                Intent intent = new Intent(getApplicationContext(), CarWashInfoActivity.class);
                 startActivityForResult(intent, sub);
             }
         });
 
-        ivReview.setOnClickListener(new View.OnClickListener() {
+        ivWriteReview.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), CarWashReviewActivity.class);
+                Intent intent = new Intent(getApplicationContext(), WriteReviewActivity.class);
                 startActivityForResult(intent, sub);
             }
         });
 
     }
-    
+
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
 
 
     void initView(){
-        ivBack = findViewById(R.id.car_wash_info_back_arrow);
-        ivReview = findViewById(R.id.car_wash_info_review_box);
+        ivBack = findViewById(R.id.car_wash_review_back_arrow);
+        ivWriteReview = findViewById(R.id.car_wash_review_write_button);
+
     }
 }
---
+++ app/src/main/java/com/kokonut/NCNC/MainActivity.java
@@ -0,0 +1,65 @@
+package com.kokonut.NCNC;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    HomeFragment homeFragment;
+    CalendarFragment calendarFragment;
+    MapFragment mapFragment;
+    CastFragment castFragment;
+    MypageFragment mypageFragment;
+
+    BottomNavigationView bottomNavigationBar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bottomNavigationBar = findViewById(R.id.bottom_navigation_bar);
+
+        homeFragment = new HomeFragment();
+        calendarFragment = new CalendarFragment();
+        mapFragment = new MapFragment();
+        castFragment = new CastFragment();
+        mypageFragment = new MypageFragment();
+
+
+        //getSupportFragmentManager().beginTransaction().replace(R.id.main_activity,HomeFragment).commitAllowingStateLoss();
+
+        bottomNavigationBar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.tab1: {
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_layout,homeFragment).commitAllowingStateLoss();
+                        return true;
+                    }
+                    case R.id.tab2: {
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_layout,calendarFragment).commitAllowingStateLoss();
+                        return true;
+                    }
+                    case R.id.tab3: {
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_layout,mapFragment).commitAllowingStateLoss();
+                        return true;
+                    }
+                    case R.id.tab4: {
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_layout,castFragment).commitAllowingStateLoss();
+                        return true;
+                    }
+                    case R.id.tab5: {
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_layout,mypageFragment).commitAllowingStateLoss();
+                        return true;
+                    }
+
+                    //default: return false;
+                }
+                return true;
+            }
+        });
+    }
+}
